33378a182bffca545d5156f9acfc330485d3e6ef
Stephen Kettley <kettleystephen@gmail.com>
2024-09-12 09:10:51 +0200

    learning mongodb crud operations

--- mongodb.js
+++ mongodb.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const uri =
   "mongodb+srv://stephenkettley:taskapp@task-app.urzzm.mongodb.net/?retryWrites=true&w=majority&appName=task-app";
@@ -10,18 +10,58 @@ async function main() {
 
     const database = client.db("task-app");
     const collection = database.collection("users");
+    const collectionTasks = database.collection("tasks");
 
-    const doc = {
-      name: "Stephen Kettley",
-      age: 27,
-    };
+    await database
+      .collection("users")
+      .updateOne(
+        {
+          _id: new ObjectId("66de975cda77b40239c45711"),
+        },
+        {
+          $inc: {
+            age: 10,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // const documents = [
+    //   { _id: new ObjectId(), name: "Abbot", age: 27 },
+    //   { _id: new ObjectId(), name: "Xolani", age: 24 },
+    // ];
 
-    // Insert the document
-    const result = await collection.insertOne(doc);
+    // const result = await collection.insertMany(documents);
+    // console.log("documents successfully inserted");
 
-    console.log(
-      `New document inserted with the following id: ${result.insertedId}`
-    );
+    // const tasks = [
+    //   { _id: new ObjectId(), description: "cry", completed: true },
+    //   { _id: new ObjectId(), description: "laugh", completed: false },
+    // ];
+
+    // const resultTask = await collectionTasks.insertMany(tasks);
+    // console.log("tasks successfully inserted into tasks collection");
+
+    // try {
+    //   const user = await database
+    //     .collection("users")
+    //     .findOne({ _id: new ObjectId("66de975cda77b40239c45711") });
+    //   console.log("User found:", user);
+    // } catch (error) {
+    //   console.log("Error occurred while fetching user:", error);
+    // }
+
+    // const users = await database
+    //   .collection("users")
+    //   .find({ age: 27 })
+    //   .toArray();
+    // console.log("users found:", users);
+  } catch (error) {
+    console.log(error);
   } finally {
     // Close the connection to the MongoDB cluster
     await client.close();
